2c220d4540303e4e45287925bee0a30d53df4d1a
Alfonso José Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2022-10-01 02:43:39 -0600

    feat: Agrega control de excepciones

--- simultaneas.py
+++ simultaneas.py
@@ -14,32 +14,35 @@ coef_row = []
 result_row = []
 variable = r + 1
 
+try:
 # Captura de datos
-for i in range(1, r + 1):
-    # Número de incógnitas
-    print(f'Ecuación {i}: ')
-    result = []
-    coef = []
-    for j in range(1, variable + 1):
-        if opc == 1:
-            x = float(input(f'x{j}: '))
-        else:
-            x = np.random.uniform(-1, 1)
-        if j < variable:
-            coef.append(x)
-        else:
-            result.append(x)
-    
-    coef_row.append(coef)
-    result_row.append(result)
+    for i in range(1, r + 1):
+        # Número de incógnitas
+        print(f'Ecuación {i}: ')
+        result = []
+        coef = []
+        for j in range(1, variable + 1):
+            if opc == 1:
+                x = float(input(f'x{j}: '))
+            else:
+                x = np.random.uniform(-1, 1)
+            if j < variable:
+                coef.append(x)
+            else:
+                result.append(x)
         
-coeficients = np.array(coef_row)
-results = np.array(result_row)
+        coef_row.append(coef)
+        result_row.append(result)
+            
+    coeficients = np.array(coef_row)
+    results = np.array(result_row)
 
-# Salida de información
-print(f'Coeficientes\n{coeficients}')
-print(f'Columna de resultados\n{results}')
+    # Salida de información
+    print(f'Coeficientes\n{coeficients}')
+    print(f'Columna de resultados\n{results}')
 
-x = np.linalg.inv(coeficients).dot(results)
+    x = np.linalg.inv(coeficients).dot(results)
 
-print(f'Solucion del sistema X= \n{x}')
+    print(f'Solucion del sistema X= \n{x}')
+except Exception as ex:
+    print(ex)
